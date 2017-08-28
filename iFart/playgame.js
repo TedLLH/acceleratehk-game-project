f7f71a5f95d59fbcfdc5230eff0a401cbbb4045c
Andrew Mcbain <mcbain.andrewt@gmail.com>
2017-08-28 11:16:29 +0800

    input update plus randel boo

--- iFart/playgame.js
+++ iFart/playgame.js
@@ -11,14 +11,14 @@ class PlayGame{
 		var tunnelBG = game.add.tileSprite(0,0,game.width,game.height,"tunnelbg");
 		tunnelBG.tint = tintColor;
 		console.log("playgame started");
-		this.shipPositions = [(game.width - TUNNEL_WIDTH)/2+32, (game.width + TUNNEL_WIDTH)/2-32]; 
-		this.ship = game.add.sprite(this.shipPositions[0],860,"ship");
-		this.ship.side = 0;
-		this.ship.anchor.set(0.5);
-		this.game.physics.enable(this.ship, Phaser.Physics.ARCADE);
-		this.ship.canMove = true;
-		game.input.onDown.add(this.moveShip, this);
-		this.smokeEmitter = game.add.emitter(this.ship.x, this.ship.y + 10,20);
+
+		//ship and parameters
+		this.ship = game.add.sprite(320,720,"ship");
+		this.ship.inputEnabled = true;
+		this.ship.anchor.set(0.5, 0);
+
+		//smoke emitter and parameters
+		this.smokeEmitter = game.add.emitter((this.ship.x ), (this.ship.y));
 		this.smokeEmitter.makeParticles("smoke");
 		this.smokeEmitter.setXSpeed(-15, 15);
 		this.smokeEmitter.setYSpeed(50, 150);
@@ -29,30 +29,8 @@ class PlayGame{
 	update(){
 		this.smokeEmitter.x = this.ship.x;
 		this.smokeEmitter.y = this.ship.y;
-	}
 
-	moveShip(){
-		if(this.ship.canMove){
-			this.ship.canMove = false;
-			this.ship.side = 1 - this.ship.side;
-			var horizontalTween = game.add.tween(this.ship).to({
-				x: this.shipPositions[this.ship.side]
-			}, SHIP_HORIZONTAL_SPEED, Phaser.Easing.Linear.None, true);
-			horizontalTween.onComplete.add(function(){
-				game.time.events.add(SHIP_MOVE_DELAY,function(){
-				this.ship.canMove = true;
-				}, this);
-			}, this);
-		}
-
-		let ghostShip = game.add.sprite(this.ship.x, this.ship.y, "ship");
-		ghostShip.alpha = 0.5;
-		ghostShip.anchor.set(0.5);
-		let ghostTween = game.add.tween(ghostShip).to({
-			alpha: 0
-		}, 350, Phaser.Easing.Linear.None, true);
-		ghostTween.onComplete.add(function(){
-			ghostShip.destroy();
-		})
+		// make the ship follow the mouse from side to side
+		this.ship.x = game.input.activePointer.position.x;
 	}
 }
