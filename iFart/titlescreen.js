bb5a12f9177ed9d966564e7dc58333d28e76dfc4
Ted <llhtheodore@gmail.com>
2017-09-01 10:08:55 +0800

    play button on top in titlescreen

--- iFart/titlescreen.js
+++ iFart/titlescreen.js
@@ -1,14 +1,5 @@
 class TitleScreen{
 	create(){
-		var playButton = game.add.button(game.width / 2, game.height - 150, "playbutton", this.startGame);
-		playButton.anchor.set(0.5);
-		var tween = game.add.tween(playButton).to({
-			width: 220,
-			height: 220
-		}, 1500, "Linear", true, 0, -1);
-		tween.yoyo(true);
-		console.log("titlescreen started");
-
 		var backgroundLayer = game.add.group();
 		var spriteLayer = game.add.group();
 		var foregroundLayer = game.add.group();
@@ -17,13 +8,20 @@ class TitleScreen{
 		var sprite = spriteLayer.create(0, 420, "sprite");
 		var foreground = foregroundLayer.create(0, 0, "foreground");
 
-		
-
 		var spriteArrives = game.add.tween(sprite);
 		spriteArrives.to({x: 260}, 3000, Phaser.Easing.Linear.None);
 		spriteArrives.start();
-	}
 
+		var playButton = game.add.button(game.width / 2, game.height - 150, "playbutton", this.startGame);
+		playButton.anchor.set(0.5);
+		var tween = game.add.tween(playButton).to({
+			width: 220,
+			height: 220
+		}, 1500, "Linear", true, 0, -1);
+		tween.yoyo(true);
+		console.log("titlescreen started");
+	}
+	
 	startGame(){
 		console.log("playButton pressed");
 		game.state.start("PlayGame");
